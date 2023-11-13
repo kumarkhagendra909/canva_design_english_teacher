a7ca6bdf62030a6912c92c5e923bcd754b008edf
kumarkhagendra909 <57476268+kumarkhagendra909@users.noreply.github.com>
2023-11-13 12:45:22 +0530

    Update settings.py

--- portfolio_english/portfolio_english/settings.py
+++ portfolio_english/portfolio_english/settings.py
@@ -131,8 +131,3 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Admin account
-# username admin
-# pass = Kumar@123
